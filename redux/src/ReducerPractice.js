e9ac3e23da4893c7d460ea5b4c395ea1ebe6613c
Surya G <surya.g@ext.saint-gobain.com>
2023-04-12 10:48:38 +0530

    usereducer with complex data

--- redux/src/ReducerPractice.js
+++ redux/src/ReducerPractice.js
@@ -1,24 +1,42 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
 export default function ReducerPractice() {
-  const initialState = {
+  const [data, setData] = useState({
     name: "",
     age: 0,
     email: "",
+  });
+  const initialState = {
+    data,
   };
   const reducerFun = (state, action) => {
     switch (action) {
       case "Update":
-        return { name: "surya", age: 24, email: "surya@gmail.com" };
+        return { name: data.name, age: data.age, email: data.email };
       case "Reset":
-        return initialState
+        return initialState;
       default:
         return state;
     }
   };
-  const [iniState, dispatch] = useReducer(reducerFun, initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((ps) => {
+      return { ...ps, [name]: value };
+    });
+  };
+  const handleSubmit = () => {
+    dispatch("Update");
+    console.log(data);
+  };
+  const [iniState, dispatch] = useReducer(reducerFun, initialState.data);
   return (
     <div>
+      <input type="text" name="name" onChange={handleChange} />
+      <input type="number" name="age" onChange={handleChange} />
+      <input type="email" name="email" onChange={handleChange} />
+      <button onClick={handleSubmit}>Submit</button>
       <div>Name :{iniState.name}</div>
       <div>Age : {iniState.age}</div>
       <div>Email: {iniState.email}</div>
